b9274fe7988e242c439e9ec5b0c76784e903cb22
mariammuawad <mariammuawad1999@gmail.com>
2020-05-29 06:18:23 +0200

    Search Product

--- Product-Review-System/Controllers/HomeController.cs
+++ Product-Review-System/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Product_Review_System.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,12 @@ namespace WebApplication4.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string searching)
         {
-            return View();
+            productreviewDBEntities5 db = new productreviewDBEntities5();
+            return View(db.products.Where(x => x.name.Contains(searching) || searching == null).ToList());
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
