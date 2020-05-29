357e9871882615f2697e0b3f06a8617dc5150ac5
mariammuawad <mariammuawad1999@gmail.com>
2020-05-30 00:24:34 +0200

    Editing some files

--- Product-Review-System/Controllers/HomeController.cs
+++ Product-Review-System/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using Product_Review_System.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,12 +8,23 @@ namespace WebApplication4.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(string searching)
+        public ActionResult Index()
         {
-            productreviewDBEntities5 db = new productreviewDBEntities5();
-            return View(db.products.Where(x => x.name.Contains(searching) || searching == null).ToList());
+            return View();
         }
 
-        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
