a02d14c83368bbc45e7e502e62260fb779a35353
mariammuawad <mariammuawad1999@gmail.com>
2020-06-04 15:02:41 +0200

    pull

---
+++ Product-Review-System/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Product_Review_System.Models
+{
+    public class Product
+    {
+        [DisplayName("Product id")]
+        public int id { get; set; }
+
+        [DisplayName("Name")]
+        public string name { get; set; }
+
+        [DisplayName("Price")]
+        public int price { get; set; }
+
+        [DisplayName("Description")]
+        public string description { get; set; }
+
+        [DisplayName("Category id")]
+        public int category_id { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
