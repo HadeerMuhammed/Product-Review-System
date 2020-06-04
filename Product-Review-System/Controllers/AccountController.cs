b6cfd32d2b3874775e4ecefa94c601c2e449fafc
HadeerMuhammed <hadeermohammed307@gmail.com>
2020-06-04 11:41:46 +0200

    Edit RegistrationForm

--- Product-Review-System/Controllers/AccountController.cs
+++ Product-Review-System/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace WebApplication4.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName,  PhoneNumber= model.PhoneNumber, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
